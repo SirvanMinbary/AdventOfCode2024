ce8fbf84b97a545d475ef5bfdb70c5e98566a453
Sirvan Minbary <s.minbary@gmail.com>
2024-12-01 12:41:46 +0100

    Add project files.

---
+++ AdventOfCode2024/InputHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Utility
+{
+    public class InputHelper
+    {
+        public static string[] ReadInput(string fileName)
+        {
+            return File.ReadAllLines(GetPath(fileName));
+        }
+
+        private static string GetPath(string fileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @$"Advent of Code/2024/{fileName}");
+        }
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility;
+
+namespace Day01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = InputHelper.ReadInput("01.txt");
+
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+
+            foreach (var line in input)
+            {
+                var split = line.Split("   ");
+                list1.Add(int.Parse(split[0]));
+                list2.Add(int.Parse(split[1]));
+            }
+
+            list1.Sort();
+            list2.Sort();
+            var sum = 0;
+            var similarity = 0;
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var left = list1[i];
+                var right = list2[i];
+                sum += Math.Abs(left - right);
+
+                var matches = list2.Count(n => n == left);
+                similarity += left * matches;
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(similarity);
+
+        }
+    }
+}
