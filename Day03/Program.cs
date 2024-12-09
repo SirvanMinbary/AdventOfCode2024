a2610e25b609391bd754f7b80e1f5526025e9f0b
Sirvan Minbary <s.minbary@gmail.com>
2024-12-10 00:42:54 +0100

    Day 3 complete, day 4 WIP

---
+++ Day03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using Utility;
+
+namespace Day03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = InputHelper.ReadInput("03.txt");
+            var sum = 0;
+
+            foreach (var line in input)
+            {
+                var matches = Regex.Matches(line, @"(mul\(\d+,\d+\))");
+
+                foreach (Match match in matches)
+                {
+                    sum += MultiplyInstruction(match);
+                }
+            }
+            Console.WriteLine(sum);
+
+            var sum2 = 0;
+            var isEnabled = true;
+            foreach (var line in input)
+            {
+                var matches = Regex.Matches(line, @"(mul\(\d+,\d+\))|(do\(\))|\b(don't\(\))");
+
+                foreach (Match match in matches)
+                {
+                    if (match.Value == "do()")
+                    {
+                        isEnabled = true;
+                    }
+                    else if (match.Value == "don't()")
+                    {
+                        isEnabled = false;
+                    }
+                    else if (isEnabled)
+                    {
+                        sum2 += MultiplyInstruction(match);
+                    }
+                }
+            }
+            Console.WriteLine(sum2);
+        }
+
+        private static int MultiplyInstruction(Match match)
+        {
+            var splitIndex = match.Value.IndexOf(',');
+            var first = int.Parse(match.Value[4..splitIndex]);
+            var second = int.Parse(match.Value[(splitIndex + 1)..^1]);
+            return first * second;
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using Utility;
+
+namespace Day04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = InputHelper.ReadInput("04.txt");
+            var word = "XMAS";
+            var directions = new int[][]
+            {
+                [-1, 0],
+                [ -1, 1 ],
+                [ 1, 0 ],
+                [ 1, 1 ],
+                [ 1, -1 ],
+                [ 0, -1 ],
+                [ -1, -1 ],
+            };
+
+            var count = 0;
+
+            for (int line = 0; line < input.Length; line++)
+            {
+                for (int column = 0; column < input[line].Length; column++)
+                {
+                    for (int d = 0; d < directions.Length; d++)
+                    {
+                        var direction = directions[d];
+                        var coords = new int[] { column + direction[0], line + direction[1] };
+                        Console.WriteLine($"{coords[0]}, {coords[1]}");
+                        if (coords[0] < 0 || coords[0] < 0 || coords[0] + word.Length > input[column].Length || coords[1] < 0 || coords[1] == input.Length)
+                        {
+                            continue;
+                        }
+
+                        var isMatch = true;
+                        for (int length = 1; length < word.Length; length++)
+                        {
+                            var letter = input[coords[1]][coords[0]];
+                            if (word[length] != letter)
+                            {
+                                isMatch = false;
+                                break;
+                            }
+                        }
+
+                        if (isMatch)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool FindWord(string[] input, string word, int x, int y, int directionX, int directionY)
+        {
+            for (int length = 0; length < word.Length; length++)
+            {
+                var letter = input[x + directionX][y + directionY];
+                if (word[length] != letter)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
